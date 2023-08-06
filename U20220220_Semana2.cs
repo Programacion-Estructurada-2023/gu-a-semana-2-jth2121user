5f260f4e230e02e6460d2c0619ade96d6bb35764
jth2121user <140464765+jth2121user@users.noreply.github.com>
2023-08-06 17:59:29 -0600

    Items de las declarasiones de variables

---
+++ U20220220_Semana2.cs
@@ -0,0 +1,114 @@
+﻿
+using System;
+
+namespace MyApp // Items 1 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+             
+            //primer variable string
+            string nombre1 = "Judith";
+
+            //Segunda variable string
+            string nombre2 = "Esperanza";
+
+            //tercera variable string
+            string apellido1 = "Del Cid";     
+
+            //cuarta variable string
+            string apellido2 = "De la O";
+            
+            //imprimir el valor de las variables
+            Console.WriteLine("Mi nombre es " + nombre1 + " " + nombre2 + " " + apellido1 + " " + apellido2);
+
+            Console.ReadKey();
+        }
+     }
+        
+ }
+
+namespace MyApp // Items 2 
+
+    internal class Program
+{
+        static void Main(string[] args)
+    {
+        
+        {
+            //declararemos variables integer(int)
+            // Variable para almacenar tu edad
+            int miEdad = 21; // Cambia el valor a tu edad actual
+
+            // Variables para almacenar las edades de tus padres
+            int edadPapa = 50; // Cambia el valor a la edad de tu padre
+            int edadMama = 56; // Cambia el valor a la edad de tu madre
+
+            // Mostrar la información en pantalla
+            Console.WriteLine("Mi edad es: 21" + miEdad);
+            Console.WriteLine("La edad de mi papa es: 50" + edadPapa);
+            Console.WriteLine("La edad de mi mama es: 56" + edadMama);
+
+            Console.ReadKey();
+        }
+     }
+        
+ }
+
+ namespace MyApp // Items 3 
+
+    internal class Program
+{
+        static void Main(string[] args)
+    {
+        
+        {
+
+            // Variables para representar gastos frecuentes
+            float gastoComida = 30.50f; // Gasto promedio diario en comida
+            float gastoTransporte = 50.75f; // Gasto semanal en transporte público
+            float gastoEntretenimiento = 20.25f; // Gasto mensual en entretenimiento
+
+            // Mostrar la información en pantalla
+            Console.WriteLine("Gasto promedio diario en comida: $" + gastoComida);
+            Console.WriteLine("Gasto semanal en transporte público: $" + gastoTransporte);
+            Console.WriteLine("Gasto mensual en entretenimiento: $" + gastoEntretenimiento);
+
+            // Pausar la consola para ver los resultados
+
+            Console.ReadKey();
+        }
+     }
+        
+ }
+
+ namespace MyApp // Items 4
+
+    internal class Program
+{
+        static void Main(string[] args)
+    {
+        
+        {
+
+            // Variables para representar el estado como estudiante en esta materia y si es becado
+            bool esEstudiante = true; // Cambia a false si no eres estudiante en esta materia
+            bool esBecado = false; // Cambia a false si no eres becado
+
+            // Mostrar la información en pantalla
+            Console.WriteLine("¿Eres estudiante en esta materia?: " + esEstudiante);
+            Console.WriteLine("¿Eres becado?: " + no es Becado);
+            // Pausar la consola para ver los resultados
+
+            Console.ReadKey();
+        }
+     }
+        
+ }
+
+           // AUTOEVALUACION: 7 
+           // NOMBRE: Judith Esperanza Del Cid De La O 
+
+
